217d2afad2c8513be7f55ec4e44133cc97213ba0
c acton <pingsucceeded@googlemail.com>
2017-05-25 16:58:00 +0100

    added sources to wallrepper

--- role.wallrepper.js
+++ role.wallrepper.js
@@ -13,15 +13,20 @@ var roleWallrepper = {
         	        creep.say('repair');
         	    }
 
-        	    if(creep.memory.repairing) {
-                var wall = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_WALL) && structure.energy < structure.energyCapacity;}});
-
-       	        if(wall.length > 0) {
-                       if(creep.repair(wall[0]) == ERR_NOT_IN_RANGE) {
-                           creep.moveTo(wall[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                       }
+        	    if(creep.memory.repairing) {    
+                    var wall = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_WALL) && structure.energy < structure.energyCapacity;}});
+    
+           	        if(wall.length > 0) {
+                           if(creep.repair(wall[0]) == ERR_NOT_IN_RANGE) {
+                               creep.moveTo(wall[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                           }
+                    }
+                }else{
+                  var sources = creep.room.find(FIND_SOURCES);
+                    if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                         creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
                 }
-              }
 
       }
 };
